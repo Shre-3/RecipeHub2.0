ffbc9212edf76e49794b3122bf4ef779b720478f
Shreya Acharya <vsa.shreya@gmail.com>
2025-04-12 12:14:57 +0530

    Updated CORS

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "http://localhost:5174",
-      "http://127.0.0.1:5174",
-    ],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
